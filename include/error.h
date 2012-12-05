43960a80637984459015280ea370672d3765aad0
Mickaël Menu <mickael.menu@gmail.com>
2012-12-05 14:06:42 +0100

    Added error handler + .gitignore

---
+++ include/error.h
@@ -0,0 +1,109 @@
+/**
+ * @file error.h
+ * Provides several macros and functions to handle errors.
+ *
+ * Senseless example of use:
+ * int main() {
+ *     ALLOC_ERROR(foo_err);
+ *     foo(1, &foo_err);
+ *     if (HAS_ERROR(foo_err))
+ *         CRASH(&foo_err); // or PRINT_ERROR(&foo_err)
+ *     return EXIT_SUCCESS;
+ * }
+ *
+ * int foo(int a, PIT_Error *error) {
+ *     int f = 0;
+ *     if (a < 0) {
+ *         NEW_ERROR(error, ERR_WRONG_ARG);
+ *     } else {
+ *         ALLOC_ERROR(bar_err);
+ *         int b = bar(-a, &bar_err);
+ *         if (HAS_ERROR(bar_err))
+ *             ADD_ERROR(error, bar_err, ERR_FOO_FAILED);
+ *         else
+ *             f = a + b;
+ *     }
+ *     return f;
+ * }
+ *
+ * int bar(int b, PIT_Error *error) {
+ *     if (b < 0)
+ *         NEW_ERROR(error, ERR_WRONG_ARG);
+ *     else
+ *         b++;
+ *     return b;
+ * }
+ */
+
+#ifndef ERROR_H
+#define ERROR_H
+
+#include <stdlib.h>
+#include <assert.h>
+
+typedef enum PIT_ErrorId PIT_ErrorId;
+typedef struct PIT_Error PIT_Error;
+
+// TO BE MODIFIED WHEN YOU ADD AN ERROR ////////////////////////////////////////
+
+// Number of errors defined.
+#define NB_ERRORS 3
+
+// List of all errors defined.
+enum PIT_ErrorId {
+	ERR_WRONG_ARG = 1,
+	ERR_FOO_FAILED,
+	ERR_BAR_FAILED
+	/* ... */
+};
+
+// List of all error messages in order.
+static const char *error_messages[NB_ERRORS + 1] = {
+	"No error",
+	"The argument is wrong",
+	"Foo failed",
+	"Bar failed"
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+// An error stack.
+struct PIT_Error {
+	PIT_ErrorId id;  // ID of the error (0 if the error didn't occured).
+	char *file;      // File where the error occured.
+	int line;        // Line in <file> where the error occured.
+	PIT_Error *prev; // Link to the previous error (NULL if this is the first).
+};
+
+// macros used to handle the errors
+#define ASSERT(test) \
+	assert(test);
+
+#define ALLOC_ERROR(error) \
+	PIT_Error error = (PIT_Error){.id = 0, .file = NULL, .line = 0, .prev = NULL}
+
+#define HAS_ERROR(error) \
+	error.id > 0
+
+#define NEW_ERROR(error, id) \
+	PIT_init_error(error, id, __FILE__, __LINE__)
+
+#define ADD_ERROR(error, prev, id) \
+	PIT_add_error(error, prev, id, __FILE__, __LINE__)
+
+#define CRASH(error) \
+	PIT_crash(error, __FILE__, __FUNCTION__)
+
+#define PRINT_ERROR(error) \
+	PIT_print_error(error, __FILE__, __FUNCTION__)
+
+// underlying error functions
+extern void PIT_init_error(PIT_Error *error, PIT_ErrorId id, const char *file, int line);
+extern void PIT_add_error(PIT_Error *error, PIT_Error prev_error, PIT_ErrorId id, const char *file, int line);
+extern PIT_Error *PIT_copy_error(const PIT_Error error);
+extern void PIT_crash(PIT_Error *error, const char *file, const char *fct);
+extern void PIT_print_error(PIT_Error *error, const char *file, const char *fct);
+extern const char *PIT_get_error_message(PIT_ErrorId id);
+extern void PIT_free_error(PIT_Error *error);
+
+#endif
---
+++ src/error.c
@@ -0,0 +1,137 @@
+/**
+ * @file error.c
+ * Provides several functions to handle errors.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "../include/error.h"
+
+/**
+ * Initializes a new error with a given error ID, file and line.
+ *
+ * @param error New error to be returned by the caller.
+ * @param file File where the error occured.
+ * @param line Line in <file> where the error occured.
+ * @return Initialized error.
+ */
+void PIT_init_error(PIT_Error *error, PIT_ErrorId id, const char *file, int line)
+{
+	if (error != NULL) {
+		error->id = id;
+		error->file = malloc(strlen(file) * sizeof(char));
+		if (error->file == NULL) {
+			perror("Unable to create a new PIT_Error");
+			exit(EXIT_FAILURE);
+		}
+		strncpy(error->file, file, strlen(file));
+		error->line = line;
+		error->prev = NULL;
+	}
+}
+
+/**
+ * Adds a new error to the error stack by appending the previous error to the
+ * new one.
+ *
+ * @param error New error to be returned by the caller.
+ * @param prev_error Previous error occured.
+ * @param file File where the error occured.
+ * @param line Line in <file> where the error occured.
+ * @return New error linked to previous error.
+ */
+void PIT_add_error(PIT_Error *error, PIT_Error prev_error, PIT_ErrorId id, const char *file, int line)
+{
+	if (error != NULL) {
+		PIT_init_error(error, id, file, line);
+		error->prev = PIT_copy_error(prev_error);
+	}
+}
+
+/**
+ * Copies a given error to a new allocated memory location.
+ *
+ * This is used to ensure the previous error in an error stack are not
+ * automatically freed.
+ *
+ * @param error Error to be copied.
+ * @param Copy of <error>.
+ */
+PIT_Error *PIT_copy_error(const PIT_Error error)
+{
+	PIT_Error *copy = malloc(sizeof(PIT_Error));
+	if (copy == NULL) {
+		fprintf(stderr, "Error: Can't create a new PIT_Error.\n");
+		exit(EXIT_FAILURE);
+	}
+	PIT_init_error(copy, error.id, error.file, error.line);
+	copy->prev = error.prev;
+	return copy;
+}
+
+/**
+ * Prints the given error stack on the standard error output and quits the
+ * program with failure return code.
+ *
+ * @param error Error stack
+ * @param file File where the crash occured (for DEBUG)
+ * @param fct Function where the crash occured (for DEBUG)
+ */
+void PIT_crash(PIT_Error *error, const char *file, const char *fct)
+{
+	PIT_print_error(error, file, fct);
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * Prints the given error stack on the standard error output. The sub-errors are
+ * freed after being printed.
+ *
+ * @param error Error stack
+ * @param file File where the print occured (for DEBUG)
+ * @param fct Function where the print occured (for DEBUG)
+ */
+void PIT_print_error(PIT_Error *error, const char *file, const char *fct)
+{
+	if (error->id > 0) {
+		fprintf(stderr, "%s: In function '%s':\n", file, fct);
+		fprintf(stderr, "%s:%d: error: %s\n", error->file, error->line, PIT_get_error_message(error->id));
+
+		if (error->prev != NULL) {
+			PIT_Error *prev = error->prev;
+			while (prev != NULL) {
+				fprintf(stderr, "%s:%d: error: %s\n", prev->file, prev->line, PIT_get_error_message(prev->id));
+				prev = prev->prev;
+			}
+
+			PIT_free_error(error->prev);
+			error->prev = NULL;
+		}
+	}
+}
+
+/**
+ * Returns the error message corresponding to the given error ID.
+ *
+ * @param id Error identifier
+ * @return Error message
+ */
+const char *PIT_get_error_message(PIT_ErrorId id)
+{
+	return error_messages[id];
+}
+
+/**
+ * Frees the memory used by an error stack.
+ *
+ * @param error Error stack.
+ */
+void PIT_free_error(PIT_Error *error)
+{
+	if (error->file != NULL)
+		free(error->file);
+	if (error->prev != NULL)
+		PIT_free_error(error->prev);
+	free(error);
+}
