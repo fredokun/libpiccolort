c147e05129fb38487e4f6b11cf5543ddcbca499d
joelhing <hing.joel@gmail.com>
2013-02-08 17:32:27 +0100

    added contracts for various functions in commitments

--- tests/pithread_test.c
+++ tests/pithread_test.c
@@ -1,5 +1,5 @@
 /**
- * @file runtime_tests.c
+ * @file pithread_tests.c
  * File that contains all the necesseray tests to check the behavior of all functions of runtime.c
  *
  * This project is released under MIT License.
